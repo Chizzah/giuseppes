57c9e58647b306a4e63ad6384c921a2103c06e8c
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2019-10-25 23:42:39 +0200

    version 0.0.3

--- src/templates/Food.vue
+++ src/templates/Food.vue
@@ -1,10 +1,5 @@
 <template>
     <div>
-      <ul>
-        <li v-for="edge in $page.allFood.edges" :key="edge.node.id">
-          {{ edge.node.title }}
-        </li>
-      </ul>
     </div>
 </template>
 
